3b9fd7cc1cec4194698df91362bb590315386780
Matt Gray <matthew.thomas.gray@gmail.com>
2011-04-06 18:58:38 +0100

    Added units of measurement

--- lander.hs
+++ lander.hs
@@ -1,9 +1,12 @@
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Simulate
+import Numeric.Units.Dimensional.Prelude
+import Numeric.Units.Dimensional.Quantities(Velocity)
+import qualified Prelude
 
 main = simulateInWindow
     "Lunar Lander"
-    (truncate screenSize, truncate screenSize)
+    (Prelude.truncate screenSize, Prelude.truncate screenSize)
     (10, 10)
     black
     30
@@ -12,47 +15,35 @@ main = simulateInWindow
     advanceWorld
 
 screenSize = 600
+pixelsPerMetre = 10 :: Int
 
-startAltitude = Altitude 5500
+startAltitude = 5500 *~ metre
 
-pixelsPerMetre = 10
+altitudeToPixels :: (Length Float) -> Float
+altitudeToPixels altitude = (altitude /~ metre) Prelude./ 10
 
-altitudeToPixels :: Altitude -> Float
-altitudeToPixels (Altitude metres) = metres / pixelsPerMetre 
+gravity = negate $ 9.81 *~ (metre / (second * second))
 
---hmm we could get some nice types in here
-gravity = -9.81
-
---keeping it 1 d so far
-data Altitude = Altitude Float deriving (Show)
-type Velocity = Float
-data Lander = Lander Altitude Velocity 
+type Altitude = Length
+data Lander = Lander (Altitude Float) (Velocity Float)
 
 worldInit :: Lander
-worldInit = Lander startAltitude 0
+worldInit = Lander startAltitude (0.0 *~ (metre/second))
 
 drawWorld :: Lander -> Picture
-drawWorld (Lander h v) = Color white $ Pictures [lander, instruments]
-	where 	lander = Translate 0 (altitudeToPixels h) $ fromGround $ Circle 5 
-		instruments = atTopLeftCorner $ Text ("alt:" ++ show h ++ " vel:" ++ show v)
+drawWorld (Lander h v) = Color white $ Pictures [(Translate 0 (altitudeToPixels h) $ fromGround $ Circle 5), (atTopLeftCorner $ Text (show h ++ " " ++ show v))]
 
 advanceWorld :: ViewPort -> Float -> Lander -> Lander
-advanceWorld _ t lander = checkNotHitGround (applyGravity lander t) t
+advanceWorld _ t lander = applyGravity lander t'
+    where t' = t *~ second
 
 fromGround :: Picture -> Picture
-fromGround p = Translate 0 (negate $ screenSize / 2) p
+fromGround p = Translate 0 (Prelude.negate $ screenSize Prelude./ 2) p
 
 atTopLeftCorner :: Picture -> Picture
 atTopLeftCorner p = Translate (-295) (250) (Scale 0.1 0.1 p)
 
---here comes the science bit
-
-applyGravity :: Lander -> Float -> Lander
-applyGravity (Lander (Altitude h) v) t = Lander h' v' where 
-	h' = Altitude (h + (v * t) + (0.5 * gravity * t * t))
+applyGravity :: Lander -> (Time Float) -> Lander
+applyGravity (Lander h v) t = Lander h' v' where
+	h' = h + (v * t) + ((_1/_2) * gravity * t * t)
 	v' = v + (gravity * t)
-
--- sort this out
-checkNotHitGround :: Lander -> Float -> Lander
-checkNotHitGround (Lander (Altitude h) v) _ = Lander h' v where h' = Altitude (max 10 h)
-
